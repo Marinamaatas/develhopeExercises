8da5eba28adc11af4fc65a31f6478cf329518fc2
Marina Matas <lmarina998@gmail.com>
2024-01-31 10:45:52 +0100

    correction exercise 4

--- JavaScript_exercises/features/rest_parameters4.js
+++ JavaScript_exercises/features/rest_parameters4.js
@@ -1,6 +1,7 @@
 /*# Rest parameters and spread syntax
 
-Perform a code refactoring by using the Spread Operator. The output of the console.log must be the same.
+Perform a code refactoring by using the Spread Operator. 
+The output of the console.log must be the same.
 
 ```
 function sum(num1, num2, num3) {
@@ -17,4 +18,4 @@ function sum(...numbers) {
 }
   
   const numbers = [1, 2, 3];
-  console.log(sum(numbers[0], numbers[1], numbers[2]));
+  console.log(sum(...numbers));
