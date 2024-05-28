7b96b3dfe35b4514922dee953b3ab8fe226d45fe
Marina Matas <lmarina998@gmail.com>
2024-05-28 18:19:09 +0200

    second exercise

---
+++ Java_Exercises/src/PrintYourNameAndSurname.java
@@ -0,0 +1,8 @@
+public class PrintYourNameAndSurname {
+    public static void main(String[] args) {
+        String myName = "Marina";
+        String mySurName = "Matas";
+        System.out.println(myName + " " + mySurName);
+    }
+
+}
