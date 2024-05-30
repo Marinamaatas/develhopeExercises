f35daa77d9845c1b90696819862a6685b4ae4ccb
Marina Matas <lmarina998@gmail.com>
2024-05-30 13:07:58 +0200

    StringPractice2

---
+++ Java_Exercises/src/StringPractice2.java
@@ -0,0 +1,22 @@
+public class StringPractice2 {
+    public static void main(String[] args) {
+        /*Define a string called string1 that has assigned the value "Hello"
+Define a string called string2 that has assigned the value "How are you?"
+Take the first 2 characters string1 and the last 2 characters of string2 and
+ combine the 4 chars (Heu?) into a dedicated array called charArray
+Print charArray*/
+        String string1 = "Hello";
+        String string2 = "How are you?";
+
+        String substring1 = string1.substring(0, 2);
+        String substring2 = string2.substring(string2.length() - 2);
+
+        StringBuilder combinedString = new StringBuilder();
+        combinedString.append(substring1);
+        combinedString.append(substring2);
+
+        char[] charArray = combinedString.toString().toCharArray();
+        
+        System.out.println(charArray);
+    }
+}
